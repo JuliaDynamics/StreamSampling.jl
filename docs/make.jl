d05f5473868c64923e784b6e979625a219d50805
Tortar <68152031+Tortar@users.noreply.github.com>
2024-01-04 00:02:35 +0100

    Try to build docs again (#10)

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,23 @@
 using Documenter
 using IteratorSampling
 
-makedocs(sitename="IteratorSampling docs")
+println("Documentation Build")
+makedocs(
+    modules = [IteratorSampling],
+    sitename = "IteratorSampling.jl",
+    pages = [
+        "API" => "api.md",
+    ],
+)
+
+@info "Deploying Documentation"
+CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
+if CI
+    deploydocs(
+        repo = "github.com/Tortar/IteratorSampling.jl.git",
+        target = "build",
+        push_preview = true,
+        devbranch = "main",
+    )
+end
+println("Finished boulding and deploying docs.")
