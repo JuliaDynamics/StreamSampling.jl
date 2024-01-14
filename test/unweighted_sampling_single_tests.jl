9a3a029e071aa3afdfc7a74d8c14bc85c9af80c6
Tortar <68152031+Tortar@users.noreply.github.com>
2024-01-14 20:45:05 +0100

    More tests for single value sampling (#27)

--- test/unweighted_sampling_single_tests.jl
+++ test/unweighted_sampling_single_tests.jl
@@ -3,6 +3,26 @@
     a, b = 1, 100
     z = itsample(a:b)
     @test a <= z <= b
-    z = itsample(Iterators.filter(x -> x != 101, a:b+1))
+    z = itsample(Iterators.filter(x -> x != b+1, a:b+1))
     @test a <= z <= b
+    rng = StableRNG(43)
+    iters = (a:b, Iterators.filter(x -> x != b + 1, a:b+1))
+    for it in iters
+        reps = 10000
+        dict_res = Dict{Int, Int}()
+        for _ in 1:reps
+            s = itsample(rng, it)
+            if s in keys(dict_res)
+                dict_res[s] += 1
+            else
+                dict_res[s] = 1
+            end
+        end
+        cases = 100
+        ps_exact = [1/cases for _ in 1:cases]
+        count_est = collect(values(dict_res))
+
+        chisq_test = ChisqTest(count_est, ps_exact)
+        @test pvalue(chisq_test) > 0.05
+    end
 end
