41844a83b414b7b344496fac1435ecb3936cace9
Tortar <68152031+Tortar@users.noreply.github.com>
2024-02-14 23:26:55 +0100

    Update UnweightedSamplingSingle.jl

--- src/UnweightedSamplingSingle.jl
+++ src/UnweightedSamplingSingle.jl
@@ -1,17 +1,25 @@
 
-function itsample(iter)
-    return itsample(Random.default_rng(), iter)
+function itsample(iter; kwargs...)
+    return itsample(Random.default_rng(), iter; kwargs...)
 end
 
-function itsample(rng::AbstractRNG, iter)
+function itsample(rng::AbstractRNG, iter; kwargs...)
     if Base.IteratorSize(iter) isa Base.SizeUnknown
-        return reservoir_sample(rng, iter)
+        return reservoir_sample(rng, iter; kwargs...)
     else 
-        return sortedindices_sample(rng, iter)
+        return sortedindices_sample(rng, iter; kwargs...)
     end
 end
 
-function reservoir_sample(rng, iter)
+function reservoir_sample(rng, iter; method = :alg_L)
+    if method === :alg_L
+        reservoir_sample(rng, iter, algL)
+    else
+        reservoir_sample(rng, iter, algR)
+    end
+end
+
+function reservoir_sample(rng, iter, alg::AlgL)
     it = iterate(iter)
     isnothing(it) && return nothing
     el, state = it
@@ -25,11 +33,25 @@ function reservoir_sample(rng, iter)
     end
 end
 
-function sortedindices_sample(rng, iter)
-    k = rand(rng, 1:length(iter))
-    for (i, x) in enumerate(iter)
-        i == k && return x
+function reservoir_sample(rng, iter, alg::AlgR)
+    it = iterate(iter)
+    isnothing(it) && return nothing
+    el, state = it
+    chosen = el
+    k = 1
+    while true
+        it = iterate(iter, state)
+        isnothing(it) && return chosen
+        state = it[2]
+        k += 1
+        rand(rng, 1:k) == 1 && (chosen = it[1])
     end
 end
 
 
+function sortedindices_sample(rng, iter; kwargs...)
+    k = rand(rng, 1:length(iter))
+    for (i, el) in enumerate(iter)
+        i == k && return el
+    end
+end
