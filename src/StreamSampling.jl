f9cfec56a4a0cb618f160cd59abd177a8d45f3b7
Tortar <68152031+Tortar@users.noreply.github.com>
2024-04-15 13:31:54 +0200

    Update and rename IteratorSampling.jl to StreamSampling.jl

---
+++ src/StreamSampling.jl
@@ -0,0 +1,114 @@
+module StreamSampling
+
+using DataStructures
+using Distributions
+using Random
+using ResumableFunctions
+using StatsBase
+
+struct WRSample end
+struct OrdWRSample end
+struct WORSample end
+struct OrdWORSample end
+
+const wrsample = WRSample()
+const ordwrsample = OrdWRSample()
+const worsample = WORSample()
+const ordworsample = OrdWORSample()
+
+abstract type AbstractReservoirSample end
+abstract type AbstractReservoirSampleMulti <: AbstractReservoirSample end
+abstract type AbstractWorReservoirSampleMulti <: AbstractReservoirSampleMulti end
+abstract type AbstractOrdWorReservoirSampleMulti <: AbstractWorReservoirSampleMulti end
+abstract type AbstractWrReservoirSampleMulti <: AbstractReservoirSampleMulti end
+abstract type AbstractOrdWrReservoirSampleMulti <: AbstractWrReservoirSampleMulti end
+
+abstract type ReservoirAlgorithm end
+
+"""
+Adapted from algorithm L described in "Random sampling with a reservoir, J. S. Vitter, 1985".
+"""
+struct AlgL <: ReservoirAlgorithm end
+
+"""
+Adapted from algorithm R described in "Random sampling with a reservoir, J. S. Vitter, 1985".
+"""
+struct AlgR <: ReservoirAlgorithm end
+
+"""
+Adapted fron algorithm RSWR_SKIP described in "Reservoir-based Random Sampling with Replacement from 
+Data Stream, B. Park et al., 2008".
+"""
+struct AlgRSWRSKIP <: ReservoirAlgorithm end
+
+"""
+Adapted from algorithm A-Res described in "Weighted random sampling with a reservoir, 
+P. S. Efraimidis et al., 2006".
+"""
+struct AlgARes <: ReservoirAlgorithm end
+
+"""
+Adapted from algorithm A-ExpJ described in "Weighted random sampling with a reservoir, 
+P. S. Efraimidis et al., 2006".
+"""
+struct AlgAExpJ <: ReservoirAlgorithm end
+
+"""
+Adapted from algorithm WRSWR_SKIP described in "A Skip-based Algorithm for Weighted Reservoir 
+Sampling with Replacement, A. Meligrana, 2024". 
+"""
+struct AlgWRSWRSKIP <: ReservoirAlgorithm end
+
+const algL = AlgL()
+const algR = AlgR()
+const algRSWRSKIP = AlgRSWRSKIP()
+const algARes = AlgARes()
+const algAExpJ = AlgAExpJ()
+const algWRSWRSKIP = AlgWRSWRSKIP()
+
+export algL, algR, algRSWRSKIP, algARes, algAExpJ, algWRSWRSKIP
+
+include("SortedSamplingSingle.jl")
+include("SortedSamplingMulti.jl")
+include("UnweightedSamplingSingle.jl")
+include("UnweightedSamplingMulti.jl")
+include("WeightedSamplingSingle.jl")
+include("WeightedSamplingMulti.jl")
+
+"""
+    itsample([rng], iter, method = algL)
+    itsample([rng], iter, wv, method = algAExpJ)
+
+Return a random element of the iterator, optionally specifying a `rng` 
+(which defaults to `Random.default_rng()`) and a `wv` function.
+If the iterator is empty, it returns `nothing`.
+
+-----
+
+    itsample([rng], iter, method = algL; ordered = false)
+    itsample([rng], iter, wv, method = algAExpJ; ordered = false)
+
+Return a vector of `n` random elements of the iterator, 
+optionally specifying a `rng` (which defaults to `Random.default_rng()`)
+and a `method`. `ordered` dictates whether an ordered sample (also called a sequential 
+sample, i.e. a sample where items appear in the same order as in `iter`) must be 
+collected.
+
+If the iterator has less than `n` elements, in the case of sampling without
+replacement, it returns a vector of those elements.
+"""
+function itsample end
+
+export itsample
+
+"""
+    sortedindices_sample(rng, iter)
+    sortedindices_sample(rng, iter, n; replace = false, ordered = false)
+
+Algorithm which generates sorted random indices used to retrieve the sample 
+from the iterable. The number of elements in the iterable needs to be known 
+before starting the sampling.
+"""
+function sortedindices_sample end
+
+end
