361b142c763b23326cdd0f4d426335ae36c56fbd
Tortar <68152031+Tortar@users.noreply.github.com>
2024-04-15 03:17:02 +0200

    Update unweighted_sampling_multi_tests.jl

--- test/unweighted_sampling_multi_tests.jl
+++ test/unweighted_sampling_multi_tests.jl
@@ -1,54 +1,46 @@
 
+
 @testset "Unweighted sampling multi tests" begin
-    combs = collect(Iterators.product(fill((true, false),2)...))
-    combs_with_m = []
-    for c in combs
-        if c[1] == false
-            push!(combs_with_m, (c..., :alg_L))
-            push!(combs_with_m, (c..., :alg_R))
-        else
-            push!(combs_with_m, (c..., nothing))
-        end
-    end
-    @testset "replace=$replace ordered=$ordered method=$method" for (replace, ordered, method) in combs_with_m
+    combs = Iterators.product([(algL, algR, algRSWRSKIP), (false, true)]...)
+    @testset "method=$method ordered=$ordered" for (method, ordered) in combs
         a, b = 1, 10
         # test return values of iter with known lengths are inrange
         iter = a:b
-        s = itsample(iter, 2, replace=replace, ordered=ordered, method=method)
+        s = itsample(iter, 2, method; ordered=ordered)
         @test length(s) == 2
         @test all(x -> a <= x <= b, s)
 
-        s = itsample(iter, 10^7, replace=replace, ordered=ordered, method=method)
-        @test replace ? length(s) == 10^7 : length(s) == 10
+        s = itsample(iter, 10^7, method; ordered=ordered)
+        @test method == algRSWRSKIP ? length(s) == 10^7 : length(s) == 10
         @test length(unique(s)) == 10
         @test all(x -> a <= x <= b, s)
 
         @test typeof(s) == Vector{Int}
-        s = itsample(iter, 2, replace=replace, ordered=ordered, method=method)
+        s = itsample(iter, 2, method; ordered=ordered)
         @test length(s) == 2
         @test all(x -> a <= x <= b, s)
         @test typeof(s) == Vector{Int}
-        s = itsample(iter, 100, replace=replace, ordered=ordered, method=method)
-        @test replace ? length(s) == 100 : length(s) == 10
+        s = itsample(iter, 100, method; ordered=ordered)
+        @test method == algRSWRSKIP ? length(s) == 100 : length(s) == 10
         @test length(unique(s)) == 10
 
         # test return values of iter with unknown lengths are inrange
         iter = Iterators.filter(x -> x < 5, a:b)
-        s = itsample(iter, 2, replace=replace, ordered=ordered, method=method)
+        s = itsample(iter, 2, method; ordered=ordered)
         @test length(s) == 2
         @test all(x -> a <= x <= b, s)
 
         @test typeof(s) == Vector{Int}
-        s = itsample(iter, 2, replace=replace, ordered=ordered, method=method)
+        s = itsample(iter, 2, method; ordered=ordered)
         @test length(s) == 2
         @test all(x -> a <= x <= b, s)
         @test typeof(s) == Vector{Int}
-        s = itsample(iter, 100, replace=replace, ordered=ordered, method=method)
-        @test replace ? length(s) == 100 : length(s) == 4
+        s = itsample(iter, 100, method; ordered=ordered)
+        @test method == algRSWRSKIP ? length(s) == 100 : length(s) == 4
         @test length(unique(s)) == 4
         @test ordered ? issorted(s) : true
 
-        rng = StableRNG(43)
+        rng = StableRNG(42)
         iters = (a:b, Iterators.filter(x -> x != b + 1, a:b+1))
         sizes = (2, 3)
         for it in iters
@@ -56,16 +48,18 @@
                 reps = 10^(size+2)
                 dict_res = Dict{Vector, Int}()
                 for _ in 1:reps
-                    s = shuffle!(rng, itsample(rng, it, size, replace=replace, ordered=ordered, method=method))
+                    s = shuffle!(rng, itsample(rng, it, size, method; ordered=ordered))
                     if s in keys(dict_res)
                         dict_res[s] += 1
                     else
                         dict_res[s] = 1
                     end
                 end
-                cases = replace ? 10^size : factorial(10)/factorial(10-size)
+                cases = method == algRSWRSKIP ? 10^size : factorial(10)/factorial(10-size)
                 ps_exact = [1/cases for _ in 1:cases]
                 count_est = collect(values(dict_res))
+
+                #println(count_est)
 
                 chisq_test = ChisqTest(count_est, ps_exact)
                 @test pvalue(chisq_test) > 0.05
