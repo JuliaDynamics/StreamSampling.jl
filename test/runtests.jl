98449875da532a787ba5096500564a9b9b8a5051
Tortar <adrianomeligrana1999@gmail.com>
2024-04-20 03:10:23 +0200

    Fix merge and test it2

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,10 +8,10 @@ using StableRNGs
 using Test
 
 @testset "StreamSampling.jl Tests" begin
-    #include("package_sanity_tests.jl")
-    #include("unweighted_sampling_single_tests.jl")
-    #include("unweighted_sampling_multi_tests.jl")
-    #include("weighted_sampling_single_tests.jl")
-    #include("weighted_sampling_multi_tests.jl")
+    include("package_sanity_tests.jl")
+    include("unweighted_sampling_single_tests.jl")
+    include("unweighted_sampling_multi_tests.jl")
+    include("weighted_sampling_single_tests.jl")
+    include("weighted_sampling_multi_tests.jl")
     include("merge_tests.jl")
 end
