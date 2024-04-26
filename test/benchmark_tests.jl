d902445f4821d90b7812ea25d5403d619bfd4482
Tortar <68152031+Tortar@users.noreply.github.com>
2024-04-26 15:19:59 +0200

    Update benchmark_tests.jl

--- test/benchmark_tests.jl
+++ test/benchmark_tests.jl
@@ -4,24 +4,23 @@
     iter = Iterators.filter(x -> x != 10, 1:10^4)
     wv(el) = 1.0
     for m in (algR, algL, algRSWRSKIP)
-    	for size in (1, 10)
-    		size == 1 && m === algRSWRSKIP && continue
-    		s = size == 1 ? () : (10,)
-	        b = @benchmark itsample($rng, $iter, s..., $m) evals=1
-	        println("Method $m sample $(size == 1 ? :single : :multi)")
-	        println("  Time: $(round(median(b.times)*1e-3, digits=2)) μs")
-	        println("  Memory: $(b.memory) bytes")
+        for size in (1, 10)
+            size == 1 && m === algRSWRSKIP && continue
+            s = size == 1 ? () : (10,)
+            b = @benchmark itsample($rng, $iter, s..., $m) evals=1
+            println("Method $m sample $(size == 1 ? :single : :multi)")
+            println("  Time: $(round(median(b.times)*1e-3, digits=2)) μs")
+            println("  Memory: $(b.memory) bytes")
         end
     end
     for m in (algARes, algAExpJ, algWRSWRSKIP)
-    	for size in (1, 10)
-    		size == 1 && m === algWRSWRSKIP && continue
-    		s = size == 1 ? () : (10,)
-	        b = @benchmark itsample($rng, $iter, $wv, s..., $m) evals=1
-	        println("Method $m sample $(size == 1 ? :single : :multi)")
-	        println("  Time: $(round(median(b.times)*1e-3, digits=2)) μs")
-	        println("  Memory: $(b.memory) bytes")
-    	end
+        for size in (1, 10)
+            size == 1 && m === algWRSWRSKIP && continue
+            s = size == 1 ? () : (10,)
+            b = @benchmark itsample($rng, $iter, $wv, s..., $m) evals=1
+            println("Method $m sample $(size == 1 ? :single : :multi)")
+            println("  Time: $(round(median(b.times)*1e-3, digits=2)) μs")
+            println("  Memory: $(b.memory) bytes")
+        end
     end
 end
-
