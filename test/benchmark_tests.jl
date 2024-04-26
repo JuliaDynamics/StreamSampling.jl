336ad3e7698a61f62214781ba6723cab431f9923
Tortar <68152031+Tortar@users.noreply.github.com>
2024-04-26 15:42:49 +0200

    Update benchmark_tests.jl

--- test/benchmark_tests.jl
+++ test/benchmark_tests.jl
@@ -6,20 +6,24 @@
         for size in (1, 10)
             size == 1 && m === algRSWRSKIP && continue
             s = size == 1 ? () : (10,)
-            b = @benchmark itsample($rng, $iter, $(s...), $m) evals=1
-            println("Method $m sample $(size == 1 ? :single : :multi)")
-            println("  Time: $(round(median(b.times)*1e-3, digits=2)) μs")
-            println("  Memory: $(b.memory) bytes")
+            b = @benchmark itsample($rng, $iter, $s..., $m) evals=1
+            mstr = "$m $(size == 1 ? :single : :multi)"
+            print(mstr * repeat(" ", 35-length(mstr)))
+            print(" --> Time: $(@sprintf("%.2f", median(b.times)*1e-3)) μs |")
+            println(" Memory: $(b.memory) bytes")
         end
+        println()
     end
     for m in (algARes, algAExpJ, algWRSWRSKIP)
         for size in (1, 10)
             size == 1 && m === algWRSWRSKIP && continue
             s = size == 1 ? () : (10,)
-            b = @benchmark itsample($rng, $iter, $wv, $(s...), $m) evals=1
-            println("Method $m sample $(size == 1 ? :single : :multi)")
-            println("  Time: $(round(median(b.times)*1e-3, digits=2)) μs")
-            println("  Memory: $(b.memory) bytes")
+            b = @benchmark itsample($rng, $iter, $wv, $s..., $m) evals=1
+            mstr = "$m $(size == 1 ? :single : :multi)"
+            print(mstr * repeat(" ", 35-length(mstr)))
+            print(" --> Time: $(@sprintf("%.2f", median(b.times)*1e-3)) μs |")
+            println(" Memory: $(b.memory) bytes")
         end
+        println()
     end
 end
