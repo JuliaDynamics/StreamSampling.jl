b78cfb2f6262fcb22d6d9618c03870a4598d9877
Tortar <68152031+Tortar@users.noreply.github.com>
2024-04-26 15:03:43 +0200

    Update benchmark_tests.jl

--- test/benchmark_tests.jl
+++ test/benchmark_tests.jl
@@ -1,18 +1,18 @@
 
 @testset "benchmarks" begin
-	rng = Xoshiro(42)
-	iter = Iterators.filter(x -> x != 10, 1:10^4)
-	wv(el) = 1.0
-	for m in (algR, algL, algRSWRSKIP)
-		b = @benchmark itsample($rng, $iter, 10, $m) evals=1
-		println("Method $m")
-		println("  Time: $(round(median(b.times)*1e-3, digits=2)) μs")
-		println("  Memory: $(b.memory) bytes")
-	end
-	for m in (algARes, algAExpJ, algWRSWRSKIP)
-		b = @benchmark itsample($rng, $iter, $wv, 10, $m) evals=1
-		println("Method $m")
-		println("  Time: $(round(median(b.times)*1e-3, digits=2)) μs")
-		println("  Memory: $(b.memory) bytes")
-	end
+    rng = Xoshiro(42)
+    iter = Iterators.filter(x -> x != 10, 1:10^4)
+    wv(el) = 1.0
+    for m in (algR, algL, algRSWRSKIP)
+        b = @benchmark itsample($rng, $iter, 10, $m) evals=1
+        println("Method $m")
+        println("  Time: $(round(median(b.times)*1e-3, digits=2)) μs")
+        println("  Memory: $(b.memory) bytes")
+    end
+    for m in (algARes, algAExpJ, algWRSWRSKIP)
+        b = @benchmark itsample($rng, $iter, $wv, 10, $m) evals=1
+        println("Method $m")
+        println("  Time: $(round(median(b.times)*1e-3, digits=2)) μs")
+        println("  Memory: $(b.memory) bytes")
+    end
 end
