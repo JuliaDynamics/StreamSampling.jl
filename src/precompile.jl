509a99ff294b1759edad61bfa47ed56194f31812
Tortar <68152031+Tortar@users.noreply.github.com>
2024-04-26 15:17:55 +0200

    Update precompile.jl

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,19 +2,20 @@
 using PrecompileTools
 
 @setup_workload begin
+    iter = Iterators.filter(x -> x != 10, 1:20);
+    wv(el) = 1.0
+    update_s!(rs, iter) = for x in iter update!(rs, x, wv(x)) end
     @compile_workload begin
-        iter = Iterators.filter(x -> x != 10, 1:20);
-        wv(el) = 1.0
         rs = ReservoirSample(Int, algARes)
-        for x in iter update!(rs, x, wv(x)) end
+        update_s!(rs, iter)
         rs = ReservoirSample(Int, algAExpJ)
-        for x in iter update!(rs, x, wv(x)) end
+        update_s!(rs, iter)
         rs = ReservoirSample(Int, 2, algARes)
-        for x in iter update!(rs, x, wv(x)) end
+        update_s!(rs, iter)
         rs = ReservoirSample(Int, 2, algAExpJ)
-        for x in iter update!(rs, x, wv(x)) end
+        update_s!(rs, iter)
         rs = ReservoirSample(Int, 2, algWRSWRSKIP)
-        for x in iter update!(rs, x, wv(x)) end
+        update_s!(rs, iter)
         itsample(iter, algR)
         itsample(iter, algL)
         itsample(iter, 2, algR)
