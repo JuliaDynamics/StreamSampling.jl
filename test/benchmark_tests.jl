0ff070c0ae85934508c1f2b467b68382932d1c3a
Tortar <68152031+Tortar@users.noreply.github.com>
2024-04-27 02:13:54 +0200

    Update benchmark_tests.jl

--- test/benchmark_tests.jl
+++ test/benchmark_tests.jl
@@ -3,22 +3,22 @@
     iter = Iterators.filter(x -> x != 10, 1:10^4)
     wv(el) = 1.0
     for m in (algR, algL, algRSWRSKIP)
-        for size in (1, 10)
-            size == 1 && m === algRSWRSKIP && continue
-            s = size == 1 ? () : (10,)
+        for size in (nothing, 10)
+            size == nothing && m === algRSWRSKIP && continue
+            s = size == nothing ? () : (size,)
             b = @benchmark itsample($rng, $iter, $s..., $m) evals=1
-            mstr = "$m $(size == 1 ? :single : :multi)"
+            mstr = "$m $(size == nothing ? :single : :multi)"
             print(mstr * repeat(" ", 35-length(mstr)))
             print(" --> Time: $(@sprintf("%.2f", median(b.times)*1e-3)) μs |")
             println(" Memory: $(b.memory) bytes")
         end
     end
     for m in (algARes, algAExpJ, algWRSWRSKIP)
-        for size in (1, 10)
-            size == 1 && m === algWRSWRSKIP && continue
-            s = size == 1 ? () : (10,)
+        for size in (nothing, 10)
+            size == nothing && m === algWRSWRSKIP && continue
+            s = size == nothing ? () : (size,)
             b = @benchmark itsample($rng, $iter, $wv, $s..., $m) evals=1
-            mstr = "$m $(size == 1 ? :single : :multi)"
+            mstr = "$m $(size == nothing ? :single : :multi)"
             print(mstr * repeat(" ", 35-length(mstr)))
             print(" --> Time: $(@sprintf("%.2f", median(b.times)*1e-3)) μs |")
             println(" Memory: $(b.memory) bytes")
