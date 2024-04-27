ea8aa16f88b6d4662838c4d8e754967b1b20b2f2
Tortar <68152031+Tortar@users.noreply.github.com>
2024-04-27 03:50:26 +0200

    Avoid aliasing problems in sortedrandrange

--- src/SortedSamplingMulti.jl
+++ src/SortedSamplingMulti.jl
@@ -62,7 +62,7 @@ end
 
 function sortedrandrange(rng, range, n)
     exp_rands = randexp(rng, n)
-    sorted_rands = accumulate!(+, exp_rands, exp_rands)
+    sorted_rands = cumsum(exp_rands)
     a, b = range.start, range.stop
     range_size = b-a+1
     cum_step = (sorted_rands[end] + randexp(rng)) / range_size
