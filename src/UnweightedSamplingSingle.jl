0ce637176b8b845547a5c03e34cf6bedf4790ad4
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-09-14 00:17:52 +0200

    Remove algorithm L for single sample

--- src/UnweightedSamplingSingle.jl
+++ src/UnweightedSamplingSingle.jl
@@ -1,42 +1,26 @@
 
-mutable struct SampleSingleAlgL{T,R} <: AbstractReservoirSampleSingle
-    state::Float64
-    seen_k::Int
-    skip_k::Int
-    const rng::R
-    value::T
-    SampleSingleAlgL{T,R}(state, seen_k, skip_k, rng) where {T,R} = new{T,R}(state, seen_k, skip_k, rng)
-end
-
 mutable struct SampleSingleAlgR{T,R} <: AbstractReservoirSampleSingle
     seen_k::Int
     skip_k::Int
     const rng::R
     value::T
-    SampleSingleAlgR{T,R}(seen_k, rng, value) where {T,R} = new{T,R}(seen_k, rng, value)
-    SampleSingleAlgR{T,R}(seen_k, rng) where {T,R} = new{T,R}(seen_k, rng)
+    SampleSingleAlgR{T,R}(rng) where {T,R} = new{T,R}(0, 0, rng)
+    SampleSingleAlgR{T,R}(seen_k, skip_k, rng, value) where {T,R} = new{T,R}(seen_k, skip_k, rng, value)
 end
 
-function value(s::SampleSingleAlgL)
-    s.state === 1.0 && return nothing
-    return s.value
-end
 function value(s::SampleSingleAlgR)
     s.seen_k === 0 && return nothing
     return s.value
 end
 
-function ReservoirSample(T, method::ReservoirAlgorithm = algR)
-    return ReservoirSample(Random.default_rng(), T, method, ms)
+function ReservoirSample(T, method::ReservoirAlgorithm = AlgR())
+    return ReservoirSample(Random.default_rng(), T, method, MutSample())
 end
-function ReservoirSample(rng::AbstractRNG, T, method::ReservoirAlgorithm = algR)
-    return ReservoirSample(rng, T, method, ms)
-end
-function ReservoirSample(rng::AbstractRNG, T, ::AlgL, ::MutSample)
-    return SampleSingleAlgL{T, typeof(rng)}(1.0, 0, 0, rng)
+function ReservoirSample(rng::AbstractRNG, T, method::ReservoirAlgorithm = AlgR())
+    return ReservoirSample(rng, T, method, MutSample())
 end
 function ReservoirSample(rng::AbstractRNG, T, ::AlgR, ::MutSample)
-    return SampleSingleAlgR{T, typeof(rng)}(0, 0, rng)
+    return SampleSingleAlgR{T, typeof(rng)}(rng)
 end
 
 @inline function update!(s::SampleSingleAlgR, el)
@@ -47,24 +31,7 @@ end
     end
     return s
 end
-@inline function update!(s::SampleSingleAlgL, el)
-    s.seen_k += 1
-    if s.skip_k > 0
-        s.skip_k -= 1
-    else
-        s.value = el
-        s.state *= rand(s.rng)
-        s.skip_k = -ceil(Int, randexp(s.rng)/log(1-s.state))
-    end
-    return s
-end
 
-function reset!(s::SampleSingleAlgL)
-    s.state = 1.0
-    s.seen_k = 0
-    s.skip_k = 0
-    return s
-end
 function reset!(s::SampleSingleAlgR)
     s.seen_k = 0
     s.skip_k = 0
@@ -75,7 +42,7 @@ function Base.merge(s1::AbstractReservoirSampleSingle, s2::AbstractReservoirSamp
     n1, n2 = n_seen(s1), n_seen(s2)
     n_tot = n1 + n2
     value = rand(s1.rng) < n1/n_tot ? s1.value : s2.value
-    return SampleSingleAlgR{typeof(value), typeof(s1.rng)}(n_tot, s1.rng, value)
+    return SampleSingleAlgR{typeof(value), typeof(s1.rng)}(n_tot, s1.skip_k + s2.skip_k, s1.rng, value)
 end
 
 function Base.merge!(s1::SampleSingleAlgR, s2::AbstractReservoirSampleSingle)
@@ -87,6 +54,7 @@ function Base.merge!(s1::SampleSingleAlgR, s2::AbstractReservoirSampleSingle)
         s1.value = s2.value
     end
     s1.seen_k = n_tot
+    s1.skip_k += s2.skip_k
     return s1
 end
 
