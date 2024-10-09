1c3d3d16472f6381b8e09a92025bd196c3011174
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-10-09 02:58:35 +0200

    Update SortedSamplingMulti.jl

--- src/SortedSamplingMulti.jl
+++ src/SortedSamplingMulti.jl
@@ -19,13 +19,13 @@ function sortedindices_sample(rng, iter, n::Int, replace;
     indices = get_sorted_indices(rng, n, N, replace)
     curr_idx, state_idx = iterate(indices)
     el, state_el = iterate(iter)::Tuple
-    for _ in 1:N-curr_idx-1
+    for _ in 1:curr_idx-1
         el, state_el = iterate(iter, state_el)::Tuple
     end
     reservoir[1] = el
     @inbounds for i in 2:n
         next_idx, state_idx = iterate(indices, state_idx)
-        for _ in 1:curr_idx-next_idx
+        for _ in 1:next_idx-curr_idx
             el, state_el = iterate(iter, state_el)::Tuple
         end
         reservoir[i] = el
