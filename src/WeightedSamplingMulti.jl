30605aea061db7a8f9ea9f7a5a49550286c31b7d
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-10-28 02:52:26 +0100

    Benchmark improvements (#109)

--- src/WeightedSamplingMulti.jl
+++ src/WeightedSamplingMulti.jl
@@ -20,6 +20,7 @@ end
 const SampleMultiOrdAlgAExpJ = Union{SampleMultiAlgAExpJ_Immut{<:OrdWeighted}, SampleMultiAlgAExpJ_Mut{<:OrdWeighted}}
 
 @hybrid struct SampleMultiAlgWRSWRSKIP{O,T,R} <: AbstractReservoirSample
+    const n::Int
     state::Float64
     skip_w::Float64
     seen_k::Int
@@ -72,17 +73,17 @@ function ReservoirSample{T}(rng::AbstractRNG, n::Integer, ::AlgARes, ::ImmutSamp
 end
 function ReservoirSample{T}(rng::AbstractRNG, n::Integer, ::AlgWRSWRSKIP, ::MutSample, ::Ord) where T
     ord = collect(1:n)
-    return SampleMultiAlgWRSWRSKIP_Mut(0.0, 0.0, 0, rng, Vector{Float64}(undef, n), Vector{T}(undef, n), ord)
+    return SampleMultiAlgWRSWRSKIP_Mut(n, 0.0, 0.0, 0, rng, Vector{Float64}(undef, n), Vector{T}(undef, n), ord)
 end
 function ReservoirSample{T}(rng::AbstractRNG, n::Integer, ::AlgWRSWRSKIP, ::MutSample, ::Unord) where T
-    return SampleMultiAlgWRSWRSKIP_Mut(0.0, 0.0, 0, rng, Vector{Float64}(undef, n), Vector{T}(undef, n), nothing)
+    return SampleMultiAlgWRSWRSKIP_Mut(n, 0.0, 0.0, 0, rng, Vector{Float64}(undef, n), Vector{T}(undef, n), nothing)
 end
 function ReservoirSample{T}(rng::AbstractRNG, n::Integer, ::AlgWRSWRSKIP, ::ImmutSample, ::Ord) where T
     ord = collect(1:n)
-    return SampleMultiAlgWRSWRSKIP_Immut(0.0, 0.0, 0, rng, Vector{Float64}(undef, n), Vector{T}(undef, n), ord)
+    return SampleMultiAlgWRSWRSKIP_Immut(n, 0.0, 0.0, 0, rng, Vector{Float64}(undef, n), Vector{T}(undef, n), ord)
 end
 function ReservoirSample{T}(rng::AbstractRNG, n::Integer, ::AlgWRSWRSKIP, ::ImmutSample, ::Unord) where T
-    return SampleMultiAlgWRSWRSKIP_Immut(0.0, 0.0, 0, rng, Vector{Float64}(undef, n), Vector{T}(undef, n), nothing)
+    return SampleMultiAlgWRSWRSKIP_Immut(n, 0.0, 0.0, 0, rng, Vector{Float64}(undef, n), Vector{T}(undef, n), nothing)
 end
 
 @inline function OnlineStatsBase._fit!(s::Union{SampleMultiAlgARes, SampleMultiOrdAlgARes}, el, w)
@@ -90,13 +91,13 @@ end
     s = @inline update_state!(s, w)
     priority = -randexp(s.rng)/w
     if s.seen_k <= n
-        push_value!(s, el, priority)
-    else
-        min_priority = last(first(s.value))
-        if priority > min_priority
-            pop!(s.value)
-            push_value!(s, el, priority)
-        end
+        @inline push_value!(s, el, priority)
+        return s
+    end
+    min_priority = last(first(s.value))
+    if priority > min_priority
+        pop!(s.value)
+        @inline push_value!(s, el, priority)
     end
     return s
 end
@@ -105,37 +106,42 @@ end
     s = @inline update_state!(s, w)
     if s.seen_k <= n
         priority = exp(-randexp(s.rng)/w)
-        push_value!(s, el, priority)
+        @inline push_value!(s, el, priority)
         if s.seen_k == n 
             s = @inline recompute_skip!(s)
         end
-    elseif s.state <= 0.0
+        return s
+    end
+    if s.state <= 0.0
         priority = @inline compute_skip_priority(s, w)
         pop!(s.value)
-        push_value!(s, el, priority)
+        @inline push_value!(s, el, priority)
         s = @inline recompute_skip!(s)
     end
     return s
 end
 @inline function OnlineStatsBase._fit!(s::SampleMultiAlgWRSWRSKIP, el, w)
-    n = length(s.value)
+    n = s.n
     s = @inline update_state!(s, w)
     if s.seen_k <= n
         @inbounds s.value[s.seen_k] = el
         @inbounds s.weights[s.seen_k] = w
         if s.seen_k == n
-            new_values = sample(s.rng, s.value, Weights(s.weights, s.state), n; ordered = is_ordered(s))
+            new_values = sample(s.rng, s.value, Weights(s.weights, s.state), n; 
+                                ordered = is_ordered(s))
             @inbounds for i in 1:n
                 s.value[i] = new_values[i]
             end
             s = @inline recompute_skip!(s, n)
             empty!(s.weights)
         end
-    elseif s.skip_w <= s.state
+        return s
+    end
+    if s.skip_w <= s.state
         p = w/s.state
         z = exp((n-4)*log1p(-p))
-        q = rand(s.rng, Uniform(z*(1-p)*(1-p)*(1-p)*(1-p),1.0))
-        k = @inline choose(n, p, q, z)
+        c = rand(s.rng, Uniform(z*(1-p)*(1-p)*(1-p)*(1-p), 1.0))
+        k = @inline choose(n, p, c, z)
         @inbounds for j in 1:k
             r = rand(s.rng, j:n)
             s.value[r], s.value[j] = s.value[j], el
@@ -182,7 +188,7 @@ function Base.merge(ss::SampleMultiAlgWRSWRSKIP...)
     skip_w = sum(getfield(s, :skip_w) for s in ss)
     state = sum(getfield(s, :state) for s in ss)
     seen_k = sum(getfield(s, :seen_k) for s in ss)
-    s = SampleMultiAlgWRSWRSKIP_Mut(state, skip_w, seen_k, ss[1].rng, Float64[], newvalue, nothing)
+    s = SampleMultiAlgWRSWRSKIP_Mut(ss[1].n, state, skip_w, seen_k, ss[1].rng, Float64[], newvalue, nothing)
     return s
 end
 
