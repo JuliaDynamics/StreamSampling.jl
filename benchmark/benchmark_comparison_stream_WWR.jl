d176153dff25660c2cc261bf950cd54d0f1856e3
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-12-16 03:14:55 +0100

    Update benchmarks (#112)
    * Update benchmarks
    
    * Update benchmark_comparison_non_stream_WWR.jl
    
    * Update benchmark_comparison_stream_WWR.jl

--- benchmark/benchmark_comparison_stream_WWR.jl
+++ benchmark/benchmark_comparison_stream_WWR.jl
@@ -85,40 +85,42 @@ end
 
 using CairoMakie
 
-f = Figure(backgroundcolor = RGBf(0.98, 0.98, 0.98), size = (1100, 700));
-
-f.title = "Comparison between AExpJ-WR and WRSWR-SKIP Algorithms"
+f = Figure(backgroundcolor = RGBf(0.98, 0.98, 0.98), size = (700, 400), dpi=1200);
 
 ax1 = Axis(f[1, 1], yscale=log10, xscale=log10, 
-       yminorticksvisible = true, yminorgridvisible = true, 
-       yminorticks = IntervalsBetween(10))
+       yminorticksvisible = true, yminorgridvisible = true, xlabelsize=16, ylabelsize=16,
+       yminorticks = IntervalsBetween(10), xticklabelsize=11, titlesize=16)
 ax2 = Axis(f[1, 2], yscale=log10, xscale=log10, 
+       yminorticksvisible = true, yminorgridvisible = true, xlabelsize=16,
+       yminorticks = IntervalsBetween(10), xticklabelsize=11, titlesize=16)
+ax3 = Axis(f[1, 3], yscale=log10, xscale=log10, xlabelsize=16,
        yminorticksvisible = true, yminorgridvisible = true, 
-       yminorticks = IntervalsBetween(10))
-ax3 = Axis(f[1, 3], yscale=log10, xscale=log10, 
-       yminorticksvisible = true, yminorgridvisible = true, 
-       yminorticks = IntervalsBetween(10))
+       yminorticks = IntervalsBetween(10), xticklabelsize=11, titlesize=16)
 
-#ax4 = Axis(f[2, 1])
+linkyaxes!(ax1, ax2, ax3)
+
+hideydecorations!(ax2, grid=false, minorgrid=false)
+hideydecorations!(ax3, grid=false, minorgrid=false)
 
 for x in benchs
-    label = x[1] == :wv_const ? (x[2] == AlgAExpJWR() ? "ExpJ-WR" : "WRSWR-SKIP") : ""
+    label = x[1] == :wv_const ? (x[2] == AlgAExpJWR() ? "A-ExpJ-WR" : "WRSWR-SKIP") : ""
     ax = x[1] == :wv_decr ? ax1 : (x[1] == :wv_const ? ax2 : ax3)
     marker = x[2] == AlgAExpJWR() ? :circle : (:xcross)
-    scatterlines!(ax, [10^i/10^8 for i in 3:7], x[3] ./ 10^6, marker = marker, 
+    scatterlines!(ax, [10^i/10^8 for i in 4:7], x[3][2:end] ./ 10^9, marker = marker, 
                   label = label, markersize = 12, linestyle = :dot)
 end
 
-Legend(ax4, labelsize=10, framevisible = false, orientation = :horizontal)
+Legend(f[2,:], ax2, labelsize=12, markersize=2, framevisible=false, orientation = :horizontal)
+rowsize!(f.layout, 1, Relative(4/5))
 
 for ax in [ax1, ax2, ax3]
     ax.xtickformat = x -> string.(round.(x.*100, digits=10)) .* "%"
     #ax.ytickformat = y -> y .* "^"
     ax.title = ax == ax1 ? "decreasing weights" : (ax == ax2 ? "constant weights" : "increasing weights")
-    ax.xticks = [10^(i)/10^8 for i in 3:7]
-    ax.yticks = [10^i for i in 2:4]
+    ax.xticks = [10^(i)/10^8 for i in 4:7]
+    ax.yticks = [10^float(i) for i in -1:1]
     ax.xlabel = "sample ratio"
-    ax == ax1 && (ax.ylabel = "time (ms)")
+    ax == ax1 && (ax.ylabel = "time (s)")
 end
 
 save("comparison_WRSWR_SKIP_alg_stream.png", f)
