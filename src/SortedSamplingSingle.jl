66a899d9a8c3c11c98bd22664853732c2fd79aa4
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-03-23 01:33:25 +0100

    Update SortedSamplingSingle.jl

--- src/SortedSamplingSingle.jl
+++ src/SortedSamplingSingle.jl
@@ -1,6 +1,6 @@
 
 function sorted_sample_single(rng, iter)
-    k = rand(s.rng, Random.Sampler(s.rng, 1:length(iter), Val(1)))
+    k = rand(rng, Random.Sampler(rng, 1:length(iter), Val(1)))
     for (i, el) in enumerate(iter)
         i == k && return el
     end
