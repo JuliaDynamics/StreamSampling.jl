9096e7cdacabe6f1ec907ae1a991e5fddd34db0b
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-08-17 16:16:12 +0200

    fix

--- src/WeightedSamplingMulti.jl
+++ src/WeightedSamplingMulti.jl
@@ -128,19 +128,18 @@ end
     s = @inline update_state!(s, w)
     if s.seen_k <= n
         @inbounds s.value[s.seen_k] = el
-        @inbounds s.weights[s.seen_k] = w
+        @inbounds s.weights[s.seen_k] = s.state
         if s.seen_k == n
-            randexps = randexp(s.rng, n)
-            ratio = s.state/(sum(randexps) + randexp(s.rng))
-            j, csweights, limit = 1, first(s.weights), 0.0
-            for i in eachindex(s.value, s.weights, randexps)
-                limit += randexps[i] * ratio
-                while csweights < limit
+            j, curx = 1, 0.0
+            newvalues = similar(s.value)
+            @inbounds for i in n:-1:1
+                curx += (1-exp(-randexp(s.rng)/i))*(1-curx)
+                while s.weights[j] < curx * s.state
                     j += 1
-                    csweights += s.weights[j]
                 end
-                s.value[i] = j
+                newvalues[i] = s.value[j]
             end
+            s.value .= newvalues
             s = @inline recompute_skip!(s, n)
         end
         return s
@@ -311,8 +310,12 @@ function OnlineStatsBase.value(s::Union{MultiAlgAResSampler, MultiAlgAExpJSample
     end
 end
 function OnlineStatsBase.value(s::MultiAlgWRSWRSKIPSampler)
+    nobs(s) == 0 && return s.value[1:0]
     if nobs(s) < length(s.value)
-        return nobs(s) == 0 ? s.value[1:0] : sample(s.rng, s.value[1:nobs(s)], weights(s.weights[1:nobs(s)]), length(s.value))
+        weightsnew = Vector{Float64}(undef, nobs(s))
+        weightsnew[1] = s.weights[1]
+        for i in 2:nobs(s) weightsnew[i] = s.weights[i] - s.weights[i-1] end
+        return sample(s.rng, s.value[1:nobs(s)], weights(weightsnew), length(s.value))
     else
         return s.value
     end
@@ -327,8 +330,12 @@ function ordvalue(s::Union{MultiOrdAlgAResSampler, MultiOrdAlgAExpJSampler})
     return first.(vals[sortperm(map(x -> x[2], vals))])
 end
 function ordvalue(s::MultiOrdAlgWRSWRSKIPSampler)
+    nobs(s) == 0 && return s.value[1:0]
     if nobs(s) < length(s.value)
-        return sample(s.rng, s.value[1:nobs(s)], weights(s.weights[1:nobs(s)]), length(s.value); ordered=true)
+        weightsnew = Vector{Float64}(undef, nobs(s))
+        weightsnew[1] = s.weights[1]
+        for i in 2:nobs(s) weightsnew[i] = s.weights[i] - s.weights[i-1] end
+        return sample(s.rng, s.value[1:nobs(s)], weights(weightsnew), length(s.value); ordered=true)
     else
         return s.value[sortperm(s.ord)]
     end
